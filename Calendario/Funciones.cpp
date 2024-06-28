a152070cbd046699b9a33be45d7666f6ff50731b
llozano <llozano@uamv.edu.ni>
2024-06-28 00:07:20 -0600

    Codigo c++

--- Calendario/Funciones.cpp
+++ Calendario/Funciones.cpp
@@ -1,51 +1,193 @@
 #include <iostream>
-#include "Variables.h"
+#include <ctime>
+#include "variables.h"
 
 using namespace std;
 
-void Calendario_Disponibilidad() {
-    
-    bool disponibilidad[NUM_HABITACIONES][NUM_DIAS]; 
+void limpiarPantalla() {
+    system("cls");
+}
 
-   
-    for (int i = 0; i < NUM_HABITACIONES; ++i) {
-        for (int j = 0; j < NUM_DIAS; ++j) {
-            disponibilidad[i][j] = true;
-        }
+bool compararFechas(const char* fecha1, const char* hora1, const char* fecha2, const char* hora2) {
+    int dia1, mes1, anio1, horaInt1, minuto1;
+    int dia2, mes2, anio2, horaInt2, minuto2;
+    
+    sscanf(fecha1, "%2d/%2d/%4d", &dia1, &mes1, &anio1);
+    sscanf(hora1, "%2d:%2d", &horaInt1, &minuto1);
+    sscanf(fecha2, "%2d/%2d/%4d", &dia2, &mes2, &anio2);
+    sscanf(hora2, "%2d:%2d", &horaInt2, &minuto2);
+
+    if (anio1 < anio2) return true;
+    if (anio1 > anio2) return false;
+    if (mes1 < mes2) return true;
+    if (mes1 > mes2) return false;
+    if (dia1 < dia2) return true;
+    if (dia1 > dia2) return false;
+    if (horaInt1 < horaInt2) return true;
+    if (horaInt1 > horaInt2) return false;
+    return minuto1 < minuto2;
+}
+
+bool validarFechaHora(const char* fecha, const char* hora) {
+    int dia, mes, anio, horaInt, minuto;
+    if (sscanf(fecha, "%2d/%2d/%4d", &dia, &mes, &anio) != 3) return false;
+    if (sscanf(hora, "%2d:%2d", &horaInt, &minuto) != 2) return false;
+    if (dia < 1 || dia > 31 || mes < 1 || mes > 12 || anio < 2023) return false;
+    if (horaInt < 0 || horaInt > 23 || minuto < 0 || minuto > 59) return false;
+    return true;
+}
+
+void obtenerFechaHoraActual(char* fecha, char* hora) {
+    time_t t = time(nullptr);
+    tm* now = localtime(&t);
+
+    sprintf(fecha, "%02d/%02d/%04d", now->tm_mday, now->tm_mon + 1, now->tm_year + 1900);
+    sprintf(hora, "%02d:%02d", now->tm_hour, now->tm_min);
+}
+
+void copiarCadena(char* destino, const char* origen, int longitud) {
+    for (int i = 0; i < longitud - 1 && origen[i] != '\0'; ++i) {
+        destino[i] = origen[i];
+    }
+    destino[longitud - 1] = '\0';
+}
+
+void registrarReserva() {
+    int apartamento;
+    char fechaInicio[11], horaInicio[6];
+    char fechaFin[11], horaFin[6];
+    double montoPagado;
+    char nombreInquilino[50];
+    char cedulaInquilino[20];
+
+    cout << "Ingrese el numero de apartamento (1-8): ";
+    cin >> apartamento;
+    if (apartamento < 1 || apartamento > 8) {
+        cout << "Numero de apartamento invalido." << endl;
+        return;
     }
 
-  
-    char opcion;
-    do {
-        int habitacion, diaInicio, diaFin;
-        cout << "Ingrese el número de habitación (1-" << NUM_HABITACIONES << "): ";
-        cin >> habitacion;
-        cout << "Ingrese el día de inicio de la reserva (1-" << NUM_DIAS << "): ";
-        cin >> diaInicio;
-        cout << "Ingrese el día de fin de la reserva (1-" << NUM_DIAS << "): ";
-        cin >> diaFin;
+    obtenerFechaHoraActual(fechaInicio, horaInicio);
+    cout << "Fecha y hora de inicio de la reserva: " << fechaInicio << " " << horaInicio << endl;
 
-       
-        for (int dia = diaInicio; dia <= diaFin; ++dia) {
-            disponibilidad[habitacion - 1][dia - 1] = false;
-        }
+    cout << "Ingrese la fecha de fin de la reserva (DD/MM/AAAA): ";
+    cin >> fechaFin;
+    cout << "Ingrese la hora de fin de la reserva (HH:MM): ";
+    cin >> horaFin;
+    if (!validarFechaHora(fechaFin, horaFin) || !compararFechas(fechaInicio, horaInicio, fechaFin, horaFin)) {
+        cout << "Fecha y hora de fin inválidas." << endl;
+        return;
+    }
 
-        cout << "¿Desea registrar otra reserva? (S/N): ";
-        cin >> opcion;
-    } while (opcion == 'S' || opcion == 's');
+    cout << "Ingrese el monto pagado: ";
+    cin >> montoPagado;
+    cout << "Ingrese el nombre del inquilino: ";
+    cin.ignore();
+    cin.getline(nombreInquilino, 50);
+    cout << "Ingrese el numero de cedula del inquilino: ";
+    cin.getline(cedulaInquilino, 20);
 
- 
-    cout << "Disponibilidad de habitaciones:" << endl;
-    for (int i = 0; i < NUM_HABITACIONES; ++i) {
-        cout << "Habitación " << i + 1 << ":" << endl;
-        for (int j = 0; j < NUM_DIAS; ++j) {
-            cout << "Día " << j + 1 << ": " << (disponibilidad[i][j] ? "Disponible" : "No disponible") << endl;
+    Reserva nuevaReserva = {apartamento};
+    copiarCadena(nuevaReserva.fechaInicio, fechaInicio, 11);
+    copiarCadena(nuevaReserva.horaInicio, horaInicio, 6);
+    copiarCadena(nuevaReserva.fechaFin, fechaFin, 11);
+    copiarCadena(nuevaReserva.horaFin, horaFin, 6);
+    nuevaReserva.montoPagado = montoPagado;
+    copiarCadena(nuevaReserva.nombreInquilino, nombreInquilino, 50);
+    copiarCadena(nuevaReserva.cedulaInquilino, cedulaInquilino, 20);
+
+    if (apartamentos[apartamento - 1].numReservas < MAX_RESERVAS) {
+        apartamentos[apartamento - 1].reservas[apartamentos[apartamento - 1].numReservas++] = nuevaReserva;
+        cout << "Reserva registrada exitosamente." << endl;
+    } else {
+        cout << "No se pueden registrar mas reservas para este apartamento." << endl;
+    }
+}
+
+void mostrarReservas() {
+    for (int i = 0; i < NUM_APARTAMENTOS; ++i) {
+        cout << "Apartamento " << i + 1 << ":" << endl;
+        for (int j = 0; j < apartamentos[i].numReservas; ++j) {
+            Reserva& reserva = apartamentos[i].reservas[j];
+            cout << "Reserva " << j + 1 << ": "
+                 << "Fecha Inicio: " << reserva.fechaInicio << " " << reserva.horaInicio
+                 << ", Fecha Fin: " << reserva.fechaFin << " " << reserva.horaFin
+                 << ", Monto Pagado: $" << reserva.montoPagado
+                 << ", Nombre Inquilino: " << reserva.nombreInquilino
+                 << ", Cedula Inquilino: " << reserva.cedulaInquilino << endl;
         }
         cout << endl;
     }
 }
 
+void eliminarReserva() {
+    int apartamento, indiceReserva;
+
+    cout << "Ingrese el numero de apartamento (1-8): ";
+    cin >> apartamento;
+    if (apartamento < 1 || apartamento > 8) {
+        cout << "Numero de apartamento inválido." << endl;
+        return;
+    }
+
+    cout << "Reservas del apartamento " << apartamento << ":" << endl;
+    for (int i = 0; i < apartamentos[apartamento - 1].numReservas; ++i) {
+        cout << i + 1 << ". Reserva desde " << apartamentos[apartamento - 1].reservas[i].fechaInicio 
+             << " " << apartamentos[apartamento - 1].reservas[i].horaInicio
+             << " hasta " << apartamentos[apartamento - 1].reservas[i].fechaFin
+             << " " << apartamentos[apartamento - 1].reservas[i].horaFin << endl;
+    }
+
+    cout << "Ingrese el numero de la reserva a eliminar: ";
+    cin >> indiceReserva;
+    if (indiceReserva < 1 || indiceReserva > apartamentos[apartamento - 1].numReservas) {
+        cout << "Numero de reserva inválido." << endl;
+        return;
+    }
+
+    for (int i = indiceReserva - 1; i < apartamentos[apartamento - 1].numReservas - 1; ++i) {
+        apartamentos[apartamento - 1].reservas[i] = apartamentos[apartamento - 1].reservas[i + 1];
+    }
+    --apartamentos[apartamento - 1].numReservas;
+    cout << "Reserva eliminada exitosamente." << endl;
+}
+
 void principal()
 {
-    Calendario_Disponibilidad();
+    char opcion;
+    do
+    {
+
+        cout << "Seleccione una opcion:\n";
+        cout << "1. Registrar nueva reserva\n";
+        cout << "2. Ver reservas registradas\n";
+        cout << "3. Eliminar una reserva\n";
+        cout << "4. Salir\n";
+        cout << "Opción: ";
+        cin >> opcion;
+
+        switch (opcion)
+        {
+        case '1':
+            system("cls");
+            registrarReserva();
+            break;
+        case '2':
+            system("cls");
+            mostrarReservas();
+            break;
+        case '3':
+            system("cls");
+            eliminarReserva();
+            break;
+        case '4':
+            return;
+        default:
+            cout << "Opcion no valida. Intente nuevamente.\n";
+            system("cls");
+        }
+        cout << "¿Desea realizar otra operacion? (S/N): ";
+        cin >> opcion;
+        system("cls");
+    } while (opcion == 'S' || opcion == 's');
 }
--- Calendario/Variables.h
+++ Calendario/Variables.h
@@ -1,2 +1,21 @@
-const int NUM_HABITACIONES = 8;
-const int NUM_DIAS = 31;
+const int NUM_APARTAMENTOS = 8;
+const int MAX_RESERVAS = 100;
+
+struct Reserva {
+    int apartamento;
+    char fechaInicio[11];
+    char horaInicio[6];   
+    char fechaFin[11];    
+    char horaFin[6];      
+    double montoPagado;
+    char nombreInquilino[50];
+    char cedulaInquilino[20];
+};
+
+struct Apartamento {
+    bool disponible;
+    Reserva reservas[MAX_RESERVAS]; 
+    int numReservas; 
+};
+
+Apartamento apartamentos[NUM_APARTAMENTOS];
