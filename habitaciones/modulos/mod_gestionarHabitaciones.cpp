2580e28c67a3178b37b65e7aa9104bd710a4576a
isaacmora999 <165749027+isaacmora999@users.noreply.github.com>
2024-07-01 23:07:51 -0600

    .mod_gestionarHabitaciones.cpp

---
+++ habitaciones/modulos/mod_gestionarHabitaciones.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+extern const int habitacionesTotales;
+extern int habitacionesOcupadas;
+extern int habitacionesDisponibles;
+extern bool habitaciones[];
+
+void OcuparHabitacion(int numHabitacion) {
+    if (numHabitacion > 0 && numHabitacion <= habitacionesTotales) {
+        if (!habitaciones[numHabitacion - 1]) {
+            habitaciones[numHabitacion - 1] = true;
+            habitacionesOcupadas++;
+            habitacionesDisponibles--;
+            cout << "Habitación " << numHabitacion << " ocupada exitosamente.\n";
+        } else {
+            cout << "La habitación " << numHabitacion << " ya está ocupada.\n";
+        }
+    } else {
+        cout << "Número de habitación inválido.\n";
+    }
+}
+
+void LiberarHabitacion(int numHabitacion) {
+    if (numHabitacion > 0 && numHabitacion <= habitacionesTotales) {
+        if (habitaciones[numHabitacion - 1]) {
+            habitaciones[numHabitacion - 1] = false;
+            habitacionesOcupadas--;
+            habitacionesDisponibles++;
+            cout << "Habitación " << numHabitacion << " liberada exitosamente.\n";
+        } else {
+            cout << "La habitación " << numHabitacion << " ya está libre.\n";
+        }
+    } else {
+        cout << "Número de habitación inválido.\n";
+    }
+}
